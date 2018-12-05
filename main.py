3c9093c1a1b72d88df6aae6a93df19468420d9fb
Paul <phunter510@gmail.com>
2018-12-05 01:14:32 -0600

    bubble sort, refresh, function improvement

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
 import tkinter as tk
 import random
+import time
 
 window = tk.Tk()
 window.title("Sorting")
@@ -9,28 +10,73 @@ canvas = tk.Canvas(window, width = 1200, height = 700)
 canvas.pack()
 
 top_x = 50
-top_y = 400
 bot_x = 55
 bot_y = 650
+max_height = 650
 
 offset = 22
 
 arr = []
 
-for x in range(50):
-    arr.append(random.randint(0,100))
-               
+def randomize(lst):
+    lst.clear()
+    for x in range(50):
+        lst.append(random.randint(0,100))
+        
+    
+##########  Quicksort ##########
+    
+def quicksort(arr, left, right):
+    if (left < right):
+        part_index = partition(arr, left, right)
+        quicksort(arr, left, part_index-1)  # Recurse using left of partition
+        quicksort(arr, part_index+1, right) # Recurse using right of partition
+
+def partition(arr, left, right):
+    pivot = arr[right]  # Choose last element of left...right as pivot
+    i = left - 1
+    for j in range(left, right):  # Iterate through element before right
+        if (arr[j] <= pivot):
+            i += 1
+            # Swap arr[i] with arr[j]
+            arr[i], arr[j] = arr[j], arr[i]
+        
+    # Swap arr[i+1] with the pivot
+    # then everything to left of pivot is <= pivot
+    # and everything to right of pivot is > pivot
+    arr[i+1], arr[right] = arr[right], arr[i+1]
+    draw_rects()
+    return i + 1
+
+def bubble_sort(lst):
+    n = len(lst)
+    for i in range(n):
+        for j in range(0,n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+            draw_rects()
+
+def left_click(event):
+    canvas.delete("all")
+
+def right_click(event):
+    bubble_sort(arr)
+    print(arr)
+
+def draw_rects():
+    canvas.delete("all")
+    for i, height in enumerate(arr):
+        rect = canvas.create_rectangle(top_x + i*offset, max_height-6*height, bot_x + i*offset, bot_y, fill = "black")
+    
 # rectange(topleft x, topleft y, bottomright x, bottomright y)
 
-for i, x in enumerate(arr):
-    rect = canvas.create_rectangle(top_x + offset, top_y, bot_x + offset, bot_y, fill = "black")
+randomize(arr)
 
+draw_rects()
 
+print(arr)
+
+window.bind("<Button-1>", left_click)
+window.bind("<Button-3>", right_click)
+        
 window.mainloop()
-
-
-
-
-
-def main():
-    pass
--- quicksort.py
+++ quicksort.py
@@ -2,9 +2,6 @@
 def quicksort(arr, left, right):
     if (left < right):
         part_index = partition(arr, left, right)
-        print("partition: ", part_index)
-        print("left: ", arr[:part_index])
-        print("right: ", arr[part_index:])
         quicksort(arr, left, part_index-1)  # Recurse using left of partition
         quicksort(arr, part_index+1, right) # Recurse using right of partition
 
@@ -16,13 +13,16 @@ def partition(arr, left, right):
             i += 1
             # Swap arr[i] with arr[j]
             arr[i], arr[j] = arr[j], arr[i]
+
+        
     # Swap arr[i+1] with the pivot
     # then everything to left of pivot is <= pivot
     # and everything to right of pivot is > pivot
     arr[i+1], arr[right] = arr[right], arr[i+1]
     return i + 1
 
-arr = [5,6,1,3,8,-212,1,-10,2,9,2,1,0]
-print(arr)
-quicksort(arr,0,len(arr)-1)
-print(arr)
+l = [54, 14, 60, 77, 75, 36, 3, 4, 100, 50, 39, 70, 47, 43, 65, 41, 37, 28, 14, 28, 22, 2, 1, 3, 1, 13, 14, 24, 76, 42, 59, 57, 33, 65, 62, 18, 9, 35, 71, 54, 43, 93, 12, 81, 31, 13, 96, 74, 63, 6]
+
+print(l)
+quicksort(l,0,len(l)-1)
+print(l)
